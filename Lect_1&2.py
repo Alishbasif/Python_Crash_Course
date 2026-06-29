3adc7f42a81699aa0ec82ee9a347fd062c72acea
Alishba Asif <alishbaasif943@gmail.com>
2026-06-29 22:25:17 +0500

    Add files via upload

---
+++ Lect_1&2.py
@@ -0,0 +1,18 @@
+# Printing our first code using print function
+print("Hello World!!")
+
+# Character Set in Python
+# 1- Using Alphabets (Upper case & Lower case)
+print("THIS IS PYTHON CRASH COURSE")
+print("this is python crash course")
+
+# 2- Using Digits (0-9)
+print("10")
+print("7")
+print("68")
+
+# Using Special Symbols(+,-,*,%,/ etc)
+print(2+3)      # Answer:5
+print(2-1)      # Answer:1
+print(4*9)      # Answer:36
+print(4/2)      # Answer:2.0
