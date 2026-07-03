f678d04dd7ee1169facbc7132b2fbd7801cc2d2a
Alishba Asif <alishbaasif943@gmail.com>
2026-07-03 22:11:08 +0500

    Add files via upload

---
+++ Lect_4.py
@@ -0,0 +1,50 @@
+# Integer
+# =========================================
+age = 23
+print("Age:", age)
+print("Data Type:", type(age))
+
+
+# =========================================
+# Float
+# =========================================
+price = 49.0
+print("Price:", price)
+print("Data Type:", type(price))
+
+
+# =========================================
+# String
+# =========================================
+name = "Ali"
+print("Name:", name)
+print("Data Type:", type(name))
+
+
+# =========================================
+# None Type
+# =========================================
+student_name = None
+print("Student Name:", student_name)
+print("Data Type:", type(student_name))
+
+# =========================================
+# Creating a Complex Number
+# =========================================
+complex_num = 2 - 8j
+
+# Display the value
+print("Complex Number:", complex_num)
+
+# Display the data type
+print("Data Type:", type(complex_num))
+
+# =========================================
+# Sum Program
+# =========================================
+a = 8
+b = 7
+
+sum = a + b
+
+print("The Sum of a and b is:", sum)
