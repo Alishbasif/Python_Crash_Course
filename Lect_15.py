ab94559ff5c3118c376793df3d6b3b55b200a4a8
Alishba Asif <alishbaasif943@gmail.com>
2026-08-08 22:23:12 +0500

    Add files via upload

---
+++ Lect_15.py
@@ -0,0 +1,27 @@
+# practice question of list and tuple question-2
+# Checking Palindrome
+
+# list_1 = [1, "abc" , "abc", 1]
+
+# copy_list_1 = list_1.copy()
+
+# copy_list_1.reverse()
+
+# if list_1 == copy_list_1:
+#     print("This is Palindrome List")
+
+# else:    
+#     print("This is not a Palindrome")
+
+# Question 3
+# Number of students have A grade
+# tup = ("C", "D", "A", "A", "B", "B", "A")
+# print("The Original Grades are: ", tup)
+# count_grade = tup.count("A")
+# print("The number of Students have the 'A' grade are: ",count_grade)
+
+
+# Question 4
+# sort A to D
+list_1 = ["C", "D", "A", "A", "B", "B", "A"]
+print(list_1.sort())
